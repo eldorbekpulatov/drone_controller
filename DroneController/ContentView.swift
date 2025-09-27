81fbcd7ada31a6e53170119413b3f708db33e77e
Eldor Bekpulatov <eldorbek0526@gmaail.com>
2025-09-27 13:29:12 -0700

    Initial Commit

---
+++ DroneController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DroneController
+//
+//  Created by Eldor Bekpulatov on 9/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DroneController/DroneControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroneControllerApp.swift
+//  DroneController
+//
+//  Created by Eldor Bekpulatov on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DroneControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
